9511de017d1b8d2066d438c0d71acff3f3004dcf
affordaweb <affordawebsolutions@gmail.com>
2026-04-07 00:19:18 -0700

    perf: defer GTM/GA to lazyOnload, remove optimizeCss (TBT fix)

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   compress: true,
   poweredByHeader: false,
-  experimental: {
-    optimizeCss: true,
-  },
 };
 
 export default nextConfig;
